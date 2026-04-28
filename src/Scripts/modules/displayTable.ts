b2b1599892f6d5182dcf707f1bb73138b1e12400
ElishreeDey <elishreed@mindfiresolutions.com>
2026-04-28 21:24:43 +0530

    remove unused code

--- src/Scripts/modules/displayTable.ts
+++ src/Scripts/modules/displayTable.ts
@@ -23,31 +23,7 @@ import type { EntryDataBase } from "./type.ts";
 
 //This function will display already entered data from browser's localstorage.
 export function displayData(){
-  //alert("on load display data");
-
-  // const storageKey = "setLocalStorageJSON";
-  // const raw = localStorage.getItem(storageKey);
-  // const allEnteredvalues: entryData[] = raw
-  //   ? JSON.parse(raw)
-  //   : [];
-
-  // const totalCount: number = allEnteredvalues.length;
-  // //const totalCount = allEnteredvalues.length;
-  // //alert(totalCount);
-
-  // //const allEnteredvalues = JSON.parse(localStorage.getItem("setLocalStorageJSON")) || []; // Parse string to array
-  // const data = JSON.stringify(allEnteredvalues);
-
-  // for (let loopCount = 0; loopCount < totalCount; loopCount++) {
-  //   const firstJSONNode = allEnteredvalues[loopCount]; // Gets one node of json object based on loop count
-  //   const firstJSONNodeString = JSON.stringify(firstJSONNode); // Convert json object to string to display in alert
-  //   //alert(firstJSONNodeString); // to access individual values
-  //   const obj = JSON.parse(firstJSONNodeString); // Convert string back to json object to access values in createTableFromData function
-
-  //   //alert(obj);
-  //   createTableFromData(obj);
-  // }     
-
+  
   const storageKey = 'setLocalStorageJSON';
   const raw = localStorage.getItem(storageKey);
     
